329d7ccb51faa6649a85bbdb4f5b3804ae2b5a9c
arty gvozdenkov <gvozdenkov@gmail.com>
2023-04-06 10:03:43 +0300

    feat: ingredient card image scale on hover

--- src/components/ingredient-card/ingredeint-image.js
+++ src/components/ingredient-card/ingredeint-image.js
@@ -1,3 +1,6 @@
+import clsx from 'clsx';
+import s from './ingredient-image.module.scss';
+
 export const IngredeintImage = ({ image, alt }) => {
-  return <img src={image} alt={alt} />;
+  return <img src={image} alt={alt} className={clsx(s.image)} />;
 };
