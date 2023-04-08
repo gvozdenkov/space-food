814a242750893cec9a5218fbf0332238ce3b4b6c
arty gvozdenkov <gvozdenkov@gmail.com>
2023-04-08 14:42:38 +0300

    feat: withLoading HOC, MainWithLoading

--- src/components/Loading/loading.js
+++ src/components/Loading/loading.js
@@ -1,20 +1,24 @@
 import clsx from 'clsx';
-import { useIntl } from 'react-intl';
 import s from './loading.module.scss';
 import ellipsis from '../../styles/blocks/animated-ellipsis/animated-ellipsis.module.scss';
+import PropTypes from 'prop-types';
+import { useIntl } from 'react-intl';
 
-export const Loading = () => {
+export const Loading = ({ title, text = '' }) => {
   const intl = useIntl();
 
   return (
     <div className={clsx(s.loading)}>
-      <h1 className={clsx(ellipsis.container, 'text text_type_main-large mb-4')}>
-        <span>{intl.formatMessage({ id: 'loading.title' })}</span>
+      <span className={clsx(ellipsis.container, 'text text_type_main-large mb-4')}>
+        <span>{title ? title : intl.formatMessage({ id: 'loading.title' })}</span>
         <span className={clsx(ellipsis.ellipsis_animate)}></span>
-      </h1>
-      <p className='text text_type_main-small text_color_inactive'>
-        {intl.formatMessage({ id: 'loading.subTitle' })}
-      </p>
+      </span>
+      {text && <p className='text text_type_main-small text_color_inactive'>{text}</p>}
     </div>
   );
+};
+
+Loading.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string,
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,15 +3,16 @@ import { Header } from '../header';
 import { Main } from '../main';
 import { useIngredients } from '../../utils/hooks/useIngredients';
 import { IngredientContext } from '../../utils/contexts/ingredientsContext';
-import { Loading } from '../Loading';
 import { Modal } from '../modal';
 import { useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
+import { withLoading } from '../../utils/hoc/withLoading';
 
 export const App = () => {
   const { data, error, loading } = useIngredients();
   const [isOpen, setIsOpen] = useState(false);
   const intl = useIntl();
+  const MainWithLoading = withLoading(Main);
 
   useEffect(() => {
     if (error) setIsOpen(true);
@@ -21,18 +22,20 @@ export const App = () => {
     <div className={s.app}>
       <Header />
       <IngredientContext.Provider value={data}>
-        
-        <Modal
-          title={intl.formatMessage({ id: 'popup.error.ingrdientsLoading.title' })}
-          open={isOpen}
-          setOpen={setIsOpen}>
-          <p className='text text_type_main-medium mt-8'>
-            {intl.formatMessage({ id: 'popup.error.ingrdientsLoading.message' })}
-          </p>
-        </Modal>
-
-        {loading ? <Loading /> : <Main />}
+        <MainWithLoading
+          isLoading={loading}
+          text={intl.formatMessage({ id: 'loading.subTitle' })}
+        />
       </IngredientContext.Provider>
+
+      <Modal
+        title={intl.formatMessage({ id: 'popup.error.ingrdientsLoading.title' })}
+        open={isOpen}
+        setOpen={setIsOpen}>
+        <p className='text text_type_main-medium mt-8'>
+          {intl.formatMessage({ id: 'popup.error.ingrdientsLoading.message' })}
+        </p>
+      </Modal>
     </div>
   );
 };
