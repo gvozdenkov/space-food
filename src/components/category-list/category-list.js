d5f3e8e8cfe7219bfad51c0e8dc97ab2d90bc6cb
arty gvozdenkov <gvozdenkov@gmail.com>
2023-04-10 12:52:31 +0300

    feat: memo ingredient list & category

--- src/components/category-list/category-list.js
+++ src/components/category-list/category-list.js
@@ -3,27 +3,29 @@ import { IngredientList } from '../ingredient-list';
 import s from './category-list.module.scss';
 import PropTypes from 'prop-types';
 import { useCategoryList } from './useCategoryList';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { IngredientContext } from '../../utils/contexts/ingredientsContext';
 
 export const CategoryList = ({ types }) => {
   const { filterByType } = useCategoryList();
   const ingredients = useContext(IngredientContext);
 
+  const ingredientList = useMemo(() => {
+    return types.map((type, index) => {
+      return (
+        <li key={index}>
+          <h2 className='text text_type_main-medium mb-6' id={`${type.type}-category`}>
+            {type.text}
+          </h2>
+          <IngredientList ingredients={filterByType(ingredients, type.type)} />
+        </li>
+      );
+    });
+  }, [types, filterByType, ingredients]);
+
   return (
     <>
-      <ul className={clsx(s.categoryList, 'customScroll')}>
-        {types.map((type, index) => {
-          return (
-            <li key={index}>
-              <h2 className='text text_type_main-medium mb-6' id={`${type.type}-category`}>
-                {type.text}
-              </h2>
-              <IngredientList ingredients={filterByType(ingredients, type.type)} />
-            </li>
-          );
-        })}
-      </ul>
+      <ul className={clsx(s.categoryList, 'customScroll')}>{ingredientList}</ul>
     </>
   );
 };
--- src/components/ingredient-list/ingredient-list.js
+++ src/components/ingredient-list/ingredient-list.js
@@ -3,20 +3,20 @@ import s from './ingredient-list.module.scss';
 import PropTypes from 'prop-types';
 import { ingredientPropTypes } from '../../utils/config';
 import { IngredientCard } from '../ingredient-card';
+import { useMemo } from 'react';
 
 export const IngredientList = ({ ingredients }) => {
+  const cards = useMemo(() => {
+    return ingredients.map((ingredient) => {
+      return (
+        <li key={ingredient._id}>
+          <IngredientCard ingredient={ingredient} />
+        </li>
+      );
+    });
+  }, [ingredients]);
 
-  return (
-    <ul className={clsx(s.ingredientList, 'mb-10')}>
-      {ingredients.map((ingredient) => {
-        return (
-          <li key={ingredient._id}>
-            <IngredientCard ingredient={ingredient} />
-          </li>
-        );
-      })}
-    </ul>
-  );
+  return <ul className={clsx(s.ingredientList, 'mb-10')}>{cards}</ul>;
 };
 
 IngredientList.propTypes = {
